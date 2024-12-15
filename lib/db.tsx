cf64f24eead6f5157786e5cbf293aa90a3943a9e
ahmwajih <ahmed.wajih.ben.smida@gmail.com>
2024-12-15 17:40:27 +0100

    2nd update

--- lib/db.tsx
+++ lib/db.tsx
@@ -8,7 +8,7 @@ const db = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI!, {
+    await mongoose.connect(process.env.MONGODB_URI!, {
     
     });
     console.log('MongoDB connected');
